f64dea27a7684d4ae38b96d9ef472aea970592d0
Levent <deineechte@mail.de>
2026-05-05 13:06:21 +0200

    Kurze Beschreibung

--- components/TicketTableView.tsx
+++ components/TicketTableView.tsx
@@ -85,6 +85,7 @@ const StatusPill: React.FC<{ status: Status }> = ({ status }) => (
     <span 
         className="status-pill"
         style={{
+            color: `var(${statusColorMap[status]})`,
             backgroundColor: statusBgColorMap[status],
             borderColor: statusBorderColorMap[status],
         }}
